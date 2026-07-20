e0203ccb786d56f9d5e3fedd91ecd4b76cb38961
kernelpenguin <lzhrgrcc@gmail.com>
2026-07-20 06:11:09 -0300

    Auto-generated commit

--- chunk-DFACKDNZ.js
+++
@@ -1,1 +0,0 @@
-import{g as a}from"./chunk-UVKGGG5C.js";import"./chunk-UO6BPTVX.js";import"./chunk-SEELAX37.js";export{a as BrandingGuidelinesComponent};
---
+++ chunk-UIPBRLSU.js
@@ -0,0 +1,1 @@
+import{g as a}from"./chunk-ZJ42425W.js";import"./chunk-SSMPOABA.js";import"./chunk-N7OGLVQ5.js";export{a as BrandingGuidelinesComponent};
